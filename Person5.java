0d157b992bdb4d4112125ef245ec9e50f0b94309
= <=>
2013-02-15 15:48:52 -0500

    Implemented features for the Person5.java class

---
+++ Person5.java
@@ -0,0 +1,61 @@
+package edu.gatech.oad.antlab.person;
+
+/**
+ *  A simple class for person 5
+ *  returns their name and a
+ *  modified string 
+ *  
+ *  @author Felipe Roriz
+ *  @version 1.1
+ */
+public class Person5 {
+  /** Holds the persons real name */
+  private String name;
+  	/**
+	 * The constructor, takes in the persons
+	 * name
+	 * @param pname the person's real name
+	 */
+  public Person5(String pname) {
+    name = pname;
+  }
+  	/**
+	 * This method should take the string
+	 * input and return its characters rotated
+	 * 3 positions.
+	 * given "gtg123b" it should return
+	 * "123bgtg".
+	 *
+	 * @param input the string to be modified
+	 * @return the modified string
+	 */
+	private String calc(String input) {
+	  //Person 5 put your implementation here
+	  String ans = "";
+	  char[] bin = input.toCharArray();
+	  for(int i = 3; i < input.length(); i++) {
+	  	ans += bin[i];
+	  }
+	  String temp = "";
+
+	  temp += bin[0];
+	  temp += bin[1];
+	  temp += bin[2];
+
+	  ans = ans + temp;
+	  return ans;
+	}
+	
+	/**
+	 * Return a string rep of this object
+	 * that varies with an input string
+	 *
+	 * @param input the varying string
+	 * @return the string representing the 
+	 *         object
+	 */
+	public String toString(String input) {
+	  return name + calc(input);
+	}
+
+}
