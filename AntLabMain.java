ad845bf2ba9c132e109ffaafc4d0421956d4696d
allichis <gth708g@mail.gatech.edu>
2013-02-15 21:34:52 -0500

    added my name and gtid

--- AntLabMain.java
+++ AntLabMain.java
@@ -63,8 +63,8 @@ import edu.gatech.oad.antlab.pkg2.*;
 		  toPrint += p1.toString("902850924");
 		  //Person2 replace P2 with your name
 		  //and ggg000x with your gt num
-		  Person2 p2 = new Person2("P2");
-		  toPrint += p2.toString("ggg002x");
+		  Person2 p2 = new Person2("Allison");
+		  toPrint += p2.toString("901922825");
 		  //Person3 replace P3 with your name
 		  //and ggg000x with your gt num
 		  Person3 p3 = new Person3("P3");
