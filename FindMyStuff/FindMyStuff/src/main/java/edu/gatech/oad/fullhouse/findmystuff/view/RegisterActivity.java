89db8c24120bf4b441221ce50316723b1ba6f209
allichis <gth708g@mail.gatech.edu>
2013-02-25 00:05:45 -0500

    added registerAttempt method to RegisterActivity class

--- FindMyStuff/FindMyStuff/src/main/java/edu/gatech/oad/fullhouse/findmystuff/view/RegisterActivity.java
+++ FindMyStuff/FindMyStuff/src/main/java/edu/gatech/oad/fullhouse/findmystuff/view/RegisterActivity.java
@@ -1,16 +1,24 @@
 package edu.gatech.oad.fullhouse.findmystuff.view;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
 import edu.gatech.oad.fullhouse.findmystuff.R;
+import edu.gatech.oad.fullhouse.findmystuff.pres.LoginPresenter;
+import edu.gatech.oad.fullhouse.findmystuff.pres.RegisterPresenter;
 
 public class RegisterActivity extends Activity {
+	
+	private RegisterPresenter presenter;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_resgister);
+		presenter = new RegisterPresenter(this);
 	}
 
 	@Override
@@ -20,4 +28,15 @@ public class RegisterActivity extends Activity {
 		return true;
 	}
 
+	public void registerAttempt(View v) {
+		String username = ((TextView)findViewById(R.id.editTextUsername)).getText().toString();
+		String email = ((TextView)findViewById(R.id.editTextEmail)).getText().toString();
+        String password = ((TextView)findViewById(R.id.editTextPassword)).getText().toString();
+        String repassword = ((TextView)findViewById(R.id.editTextRePassword)).getText().toString();
+        if(password.equals(repassword)){
+        	presenter.checkRegInfon(username, password, "", email, "");
+        } else{
+        	// TODO display password mismatch error
+        }
+    }
 }
