1c57ca6fed7107c43e0aa6cfdbb33f5bfa09c524
Jesse Rosalia <jesse.rosalia@gatech.edu>
2013-02-28 10:03:19 -0500

    Fixed package issues from refactor in main project

--- FindMyStuff/FindMyStuff-it/src/main/java/edu/gatech/oad/fullhouse/findmystuff/test/HelloAndroidActivityTest.java
+++ FindMyStuff/FindMyStuff-it/src/main/java/edu/gatech/oad/fullhouse/findmystuff/test/HelloAndroidActivityTest.java
@@ -2,6 +2,7 @@ package edu.gatech.oad.fullhouse.findmystuff.test;
 
 import android.test.ActivityInstrumentationTestCase2;
 import edu.gatech.oad.fullhouse.findmystuff.*;
+import edu.gatech.oad.fullhouse.findmystuff.view.HelloAndroidActivity;
 
 public class HelloAndroidActivityTest extends ActivityInstrumentationTestCase2<HelloAndroidActivity> {
 
