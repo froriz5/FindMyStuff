55c3b9533f54ada487880d88e5c890213aca3155
Jesse Rosalia <jesse.rosalia@gatech.edu>
2013-04-05 08:38:51 -0400

    Fixed relation in item.rb

--- server/app/models/item.rb
+++ server/app/models/item.rb
@@ -3,5 +3,5 @@ class Item < ActiveRecord::Base
     has_and_belongs_to_many :item_features
 
     belongs_to :incident
-    delegate :incidentDate, :to => :incident
+    delegate :incident_date, :to => :incident
   end
