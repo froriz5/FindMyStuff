f21914a05592a0a677a142137e0b1920baa3e824
allichis <gth708g@mail.gatech.edu>
2013-04-07 19:18:29 -0400

    added error display methods

--- FindMyStuff/FindMyStuff/src/main/java/edu/gatech/oad/fullhouse/findmystuff/view/RegisterActivity.java
+++ FindMyStuff/FindMyStuff/src/main/java/edu/gatech/oad/fullhouse/findmystuff/view/RegisterActivity.java
@@ -6,6 +6,7 @@ import android.view.Menu;
 import android.view.View;
 import android.widget.TextView;
 import edu.gatech.oad.fullhouse.findmystuff.R;
+import edu.gatech.oad.fullhouse.findmystuff.dao.impl.ServerAccessorFactory;
 import edu.gatech.oad.fullhouse.findmystuff.pres.RegisterPresenter;
 
 public class RegisterActivity extends Activity {
@@ -16,7 +17,7 @@ public class RegisterActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_register);
-		presenter = new RegisterPresenter(this);
+		presenter = new RegisterPresenter(this, ServerAccessorFactory.getUserAccessor());
 	}
 
 	@Override
@@ -27,6 +28,14 @@ public class RegisterActivity extends Activity {
 	}
 
 	public void registerAttempt(View v) {
+		((TextView)findViewById(R.id.registerUsernameError)).setVisibility(View.GONE);
+		((TextView)findViewById(R.id.registerUsernameFormatError)).setVisibility(View.GONE);	
+        ((TextView)findViewById(R.id.registerEmailError)).setVisibility(View.GONE);
+        ((TextView)findViewById(R.id.registerEmailFormatError)).setVisibility(View.GONE);
+        ((TextView)findViewById(R.id.registerPasswordMismatchError)).setVisibility(View.GONE);
+        ((TextView)findViewById(R.id.registerPasswordLengthError)).setVisibility(View.GONE);
+
+		
 		String name = ((TextView)findViewById(R.id.editTextName)).getText().toString();
 		String city = ((TextView)findViewById(R.id.editTextCity)).getText().toString();
 		String state = ((TextView)findViewById(R.id.editTextState)).getText().toString();
@@ -35,22 +44,34 @@ public class RegisterActivity extends Activity {
 		String email = ((TextView)findViewById(R.id.editTextEmail)).getText().toString();
         String password = ((TextView)findViewById(R.id.editTextPassword)).getText().toString();
         String repassword = ((TextView)findViewById(R.id.editTextRePassword)).getText().toString();
-        if(password.equals(repassword)){
-        	presenter.checkRegInfo(username, password, name, location, email, "Phone");
-        } else {
-        	displayPasswordMismatchError();
-        }
+       
+       	presenter.checkRegInfo(username, password, repassword, name, location, email, "Phone");
+     
     }
 	
 	public void displayUsernameTakenError() {
-		
+		((TextView)findViewById(R.id.registerUsernameError)).setVisibility(View.VISIBLE);
+	}
+	
+	public void displayUsernameFormatError() {
+		((TextView)findViewById(R.id.registerUsernameFormatError)).setVisibility(View.VISIBLE);		
 	}
 	
 	public void displayEmailTakenError() {
-		
+		((TextView)findViewById(R.id.registerEmailError)).setVisibility(View.VISIBLE);
+	}
+	
+	public void displayEmailFormatError() {
+		((TextView)findViewById(R.id.registerEmailFormatError)).setVisibility(View.VISIBLE);		
 	}
 	
 	public void displayPasswordMismatchError() {
-		
+		((TextView)findViewById(R.id.registerPasswordMismatchError)).setVisibility(View.VISIBLE);
 	}
+
+	public void displayPasswordLengthError() {
+		((TextView)findViewById(R.id.registerPasswordLengthError)).setVisibility(View.VISIBLE);		
+	}
+	
+	
 }
